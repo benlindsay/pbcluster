ee509dead5118804ed9e3ca90bd1f98d6e705cc1
Ben Lindsay <benjlindsay@gmail.com>
2019-05-02 08:53:17 -0400

    changed requirements_dev.txt to requirements.txt and specified lower bound for networkx

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = [
-    "networkx",
+    "networkx>=2.1",
     "numpy",
     "pandas",
     "plotnine",
